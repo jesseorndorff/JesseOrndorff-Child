f859f57b567ace35cdfae05ab79c73fa59672298
Jesse Orndorff <jesse@jesseorndorff.com>
2015-07-08 09:24:31 +0700

    basic style update

--- jesseorndorff-child/functions.php
+++ jesseorndorff-child/functions.php
@@ -7,7 +7,7 @@ URL: htp://simonbouchard.com
 /**
  *
  * Unload parent theme sytlesheets
- * 
+ *
  */
 function child_unload_styles() {
     wp_deregister_style( 'stylesheet' );
@@ -17,10 +17,10 @@ add_action( 'wp_print_styles', 'child_unload_styles', 100 );
 /**
  *
  * Load child theme stylesheet
- * 
+ *
  */
 function child_theme_styles() {
-	wp_register_style( 'stylesheet-child', get_stylesheet_directory_uri() . '/style.css' );
+	wp_register_style( 'stylesheet-child', get_stylesheet_directory_uri() . '/custom-style.css' );
 	wp_enqueue_style( 'stylesheet-child' );
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_styles', 101 );
